6c9fb9b7cebb9c2ea6350eebdd49c0c3b2178ac0
Rich Tillman <richtillman@gmail.com>
2024-07-06 01:38:31 -0400

    feat: generated UI lib and Storybook

---
+++ libs/shared/ui/src/lib/button.stories.tsx
@@ -0,0 +1,24 @@
+import { expect } from '@storybook/jest'
+import type { Meta, StoryObj } from '@storybook/react'
+import { within } from '@storybook/testing-library'
+
+import { Button } from './button'
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Button'
+}
+export default meta
+type Story = StoryObj<typeof Button>
+
+export const Primary = {
+  args: {}
+}
+
+export const Heading: Story = {
+  args: {},
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    expect(canvas.getByText(/Welcome to Button!/gi)).toBeTruthy()
+  }
+}
